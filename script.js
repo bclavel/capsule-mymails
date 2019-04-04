d56f93cb6be6ca5ef61e6330e49b6822fd9565d7
Benoit Clavel <benoitclavel@yahoo.fr>
2019-04-04 17:20:14 +0200

    fin jour 3 mymails

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log('Début')
+// Bienvenue dans le script MyMails
 
 // Définit le compteur de messages
-var totalMessages = document.getElementsByClassName('box').length
-document.getElementById('calcul-counter-texte').textContent = totalMessages
+var totalMessages = document.getElementsByClassName('box').length;
+document.getElementById('calcul-counter-texte').textContent = totalMessages;
 
 // Fonction qui met à jour le compteur
 function updateCounter() {
   document.getElementById('calcul-counter-texte').textContent = totalMessages
-}
+};
 
 // Boucle qui repère les class trash et supprime la div parent*2 au clic sur le picto
 function updateTrash() {
@@ -23,30 +23,47 @@ function updateTrash() {
     );
   };
 };
-
 updateTrash()
 
-console.log('Fin')
+// Définition du contenu du champ message
+// var messagePerso = document.getElementById('message-field').value;
 
-// document.getElementById('noel').textContent = 'Bernard Paganelli'
+// Fonction qui vient remplacer le contenu du paragraphe cible par le contenu du champ
+function updateMessage() {
+  document.getElementById('targetMessage').innerHTML = document.getElementById('message-field').value;
+  document.getElementById('noel').innerHTML = document.getElementById('name-field').value;
+};
+
+// Ecoute les clics sur le bouton Ajouter et créé un nouveau bloc avec le contenu du message
+document.getElementById('button').addEventListener("click",
+function () {
+  newContent();
+  // messagePerso = document.getElementById('message-field').value;
+  totalMessages = document.getElementsByClassName('box').length;
+  updateMessage()
+  console.log(document.getElementById('targetName'));
+  updateCounter()
+  updateTrash()
+  }
+);
 
 // Récupère le contenu du bloc de référence à duppliquer
 var contenuNoel = document.getElementById('noelContent').innerHTML;
+console.log(contenuNoel)
 
 // Créer un nouveau bloc de contenu avec le contenu duppliqué
 function newContent() {
+  var contenuNoel = document.getElementById('noelContent').innerHTML;
   document.getElementById('main-messages').prepend(document.createElement('div'));
   document.getElementById('main-messages').firstChild.classList.add('box');
   document.getElementById('main-messages').firstChild.classList.add('newMessage');
   document.getElementById('main-messages').firstChild.innerHTML = contenuNoel;
-}
+};
 
-// Ecoute et ajoute un bloc au clic + update counter
+// Ecoute et ajoute un bloc au clic sur le bouton Nouveau + update counter
 document.getElementById('new').addEventListener("click",
   function() {
     newContent();
-    // console.log('new !')
-    // console.log(document.getElementsByClassName('trash').length);
     totalMessages = document.getElementsByClassName('box').length;
     updateCounter()
     updateTrash()
